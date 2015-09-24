1ea73e5828dc0e9dfa445f8bfef2ae04dfae8c58
rogeriop <pinheiroxrogerio@gmail.com>
2015-09-24 11:17:25 -0300

    fim 05 - Conversao e validacao de dados

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -3,8 +3,12 @@ package br.com.caelum.livraria.bean;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -51,11 +55,22 @@ public class LivroBean implements Serializable {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			//throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor."));
+			return;
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
 		this.livro = new Livro();
+	}
+	
+	
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+		String valor = value.toString();
+		if (!valor.startsWith("1"))		{
+			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+			
+		}
 	}
 
 }
