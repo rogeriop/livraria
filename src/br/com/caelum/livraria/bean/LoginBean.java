18aca3c35f24773ddf37d00c2b5bd60dc7bcb8c2
rogeriop <rogeriop>
2016-06-28 16:17:05 -0300

    fim 11 - Autenticacao do usuario

---
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -0,0 +1,29 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.caelum.livraria.dao.UsuarioDao;
+import br.com.caelum.livraria.modelo.Usuario;
+
+@ManagedBean
+@ViewScoped
+public class LoginBean {
+
+	private Usuario usuario = new Usuario();
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public String efetuarLogin() {
+		System.out.println("Efetuando login do usuario" + this.usuario.getEmail());
+		
+		boolean existe = new UsuarioDao().existe(this.usuario);
+		if (existe) {
+			return "livro?faces-redirect=true";
+		}
+		
+		return null;
+	}
+}
