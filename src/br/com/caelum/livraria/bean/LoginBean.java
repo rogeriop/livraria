b9bb2bf8b6628437fe80da6bee9bd243ce8b2307
rogeriop <rogeriop>
2016-06-29 17:13:23 -0300

    fim 13 - Logout e o escopo flash

--- src/br/com/caelum/livraria/bean/LoginBean.java
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -1,7 +1,9 @@
 package br.com.caelum.livraria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -19,11 +21,24 @@ public class LoginBean {
 	public String efetuarLogin() {
 		System.out.println("Efetuando login do usuario" + this.usuario.getEmail());
 		
+		FacesContext context = FacesContext.getCurrentInstance();
 		boolean existe = new UsuarioDao().existe(this.usuario);
+
 		if (existe) {
+			context.getExternalContext().getSessionMap()
+				.put("usuarioLogado", this.usuario);
 			return "livro?faces-redirect=true";
 		}
 		
-		return null;
+		context.addMessage(null, new FacesMessage("Usuario nao encontrado"));
+		return "login";
+	}
+	
+	public String deslogar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap()
+		.remove("usuarioLogado");
+		return "login?faces-redirect=true";
+		
 	}
 }
